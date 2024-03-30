52a36939dbf4e15863fce435e60533c75ac554e0
bonj4 <omerfarukkaraboga@gmail.com>
2024-03-30 15:13:14 +0300

    add utils.py

---
+++ utils.py
@@ -0,0 +1,46 @@
+
+import torch
+from torch.utils.tensorboard import SummaryWriter
+import albumentations as A
+
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+ALL_DIR = "/content/drive/MyDrive/Colab_Notebooks/Gans/face frontalization/data/"
+LEARNING_RATE = 2e-4
+BATCH_SIZE = 16
+NUM_WORKERS = 8
+IMAGE_SIZE = 256
+CHANNELS_IMG = 3
+L1_LAMBDA = 100
+LAMBDA_GP = 10
+NUM_EPOCHS = 50 #500
+LOAD_MODEL = False
+SAVE_MODEL = True
+CHECKPOINT_DISC = "disc.pth.tar"
+CHECKPOINT_GEN = "gen.pth.tar"
+weigths_paths="/content/drive/MyDrive/Colab_Notebooks/Gans/face frontalization/weigts/"
+
+writer_real = SummaryWriter(f"{weigths_paths}/logs/real")
+writer_fake = SummaryWriter(f"{weigths_paths}/logs/fake")
+
+torch.backends.cudnn.benchmark = True
+
+both_transform = A.Compose(
+    [A.Resize(width=IMAGE_SIZE, height=IMAGE_SIZE),], additional_targets={"image0": "image"},
+)
+
+transform_only_input = A.Compose(
+    [
+        A.HorizontalFlip(p=0.5),
+        A.ColorJitter(p=0.2),
+        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
+        ToTensorV2(),
+    ]
+)
+
+transform_only_mask = A.Compose(
+    [
+        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
+        ToTensorV2(),
+    ]
+)
