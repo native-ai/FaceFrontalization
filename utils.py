adc9862cb300d2b258f3fb6fd086d0aca4bcc231
bonj4 <omerfarukkaraboga@gmail.com>
2024-04-06 14:09:42 +0300

    madify utils

--- utils.py
+++ utils.py
@@ -1,46 +1,22 @@
-
 import torch
-from torch.utils.tensorboard import SummaryWriter
-import albumentations as A
+from config import DEVICE
 
 
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-ALL_DIR = "/content/drive/MyDrive/Colab_Notebooks/Gans/face frontalization/data/"
-LEARNING_RATE = 2e-4
-BATCH_SIZE = 16
-NUM_WORKERS = 8
-IMAGE_SIZE = 256
-CHANNELS_IMG = 3
-L1_LAMBDA = 100
-LAMBDA_GP = 10
-NUM_EPOCHS = 50 #500
-LOAD_MODEL = False
-SAVE_MODEL = True
-CHECKPOINT_DISC = "disc.pth.tar"
-CHECKPOINT_GEN = "gen.pth.tar"
-weigths_paths="/content/drive/MyDrive/Colab_Notebooks/Gans/face frontalization/weigts/"
+def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
+    print("=> Saving checkpoint")
+    checkpoint = {
+        "state_dict": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+    }
+    torch.save(checkpoint, filename)
 
-writer_real = SummaryWriter(f"{weigths_paths}/logs/real")
-writer_fake = SummaryWriter(f"{weigths_paths}/logs/fake")
+def load_checkpoint(checkpoint_file, model, optimizer, lr):
+    print("=> Loading checkpoint")
+    checkpoint = torch.load(checkpoint_file, map_location=DEVICE)
+    model.load_state_dict(checkpoint["state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
 
-torch.backends.cudnn.benchmark = True
-
-both_transform = A.Compose(
-    [A.Resize(width=IMAGE_SIZE, height=IMAGE_SIZE),], additional_targets={"image0": "image"},
-)
-
-transform_only_input = A.Compose(
-    [
-        A.HorizontalFlip(p=0.5),
-        A.ColorJitter(p=0.2),
-        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
-        ToTensorV2(),
-    ]
-)
-
-transform_only_mask = A.Compose(
-    [
-        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
-        ToTensorV2(),
-    ]
-)
+    # If we don't do this then it will just have learning rate of old checkpoint
+    # and it will lead to many hours of debugging \:
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
